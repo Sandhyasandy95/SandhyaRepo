ae97533464de931c01aba0a2c922d35dcf659649
Sandhyasandy95 <cvnagasandhya95@gmail.com>
2019-05-25 15:27:10 +0530

    Trail added folder

---
+++ src/com/demo/Sample.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+class hello{
+    void m()
+    {
+        System.out.println("hii method");
+    }
+}
+public class Sample {
+
+    public static void main(String[] args) {
+        hello h=new hello();
+        h.m();
+        System.out.println("hii im sandhya");
+    }
+}
